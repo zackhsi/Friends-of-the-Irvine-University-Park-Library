4f99d2040ce47d665ba4f31b50812943249fc22c
Zack Hsi <zackhsi@gmail.com>
2013-03-06 12:11:56 -0800

    ..

--- js/navigate.js
+++ js/navigate.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){//executed after the page has loaded
+    alert("start");
     checkURL();//check if the URL has a reference to a page and load it
     $('ul li a').click(function (e){//traverse through all our navigation links..
-	alert("check");
 	checkURL(this.hash);//.. and assign them a new onclick event, using their own hash as a parameter (#page1 for example)
     });
     setInterval("checkURL()",250);//check for a change in the URL every 250 ms to detect if the history buttons have been used
@@ -15,7 +15,6 @@ function checkURL(hash)
     if(hash != lasturl)// if the hash value has changed
     {
 	lasturl=hash;//update the current hash
-	alert("hash is: " + hash);
 	loadPage(hash);// and load the new page
     }
 }
@@ -27,7 +26,7 @@ function loadPage(url)//the function that loads pages via AJAX
     alert("url: " + url);
     $.ajax({//create an ajax request to load_page.php
 	type: "POST",
-	url: "http://localhost:8080/load_page.php",
+	url: "load_page.php",
 	data: 'page='+url,//with the page number as a parameter
 	dataType: "html",//expect html to be returned
 	success: function(msg){
@@ -36,6 +35,11 @@ function loadPage(url)//the function that loads pages via AJAX
 	    {
 		$('#pageContent').html(msg);//load the returned html into pageContet
 		// $('#loading').css('visibility','hidden');//and hide the rotating gif
+	    }
+	},
+	error:function (xhr, ajaxOptions, thrownError){
+	    if(xhr.status==404) {
+		alert(thrownError);
 	    }
 	}
     });
