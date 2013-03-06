6c95b9f57e5e6989961138173cee7f1d9dea9628
Zack Hsi <zackhsi@gmail.com>
2013-03-06 08:37:10 -0800

    adding files

--- js/navigate.js
+++ js/navigate.js
@@ -27,7 +27,7 @@ function loadPage(url)//the function that loads pages via AJAX
     alert("url: " + url);
     $.ajax({//create an ajax request to load_page.php
 	type: "POST",
-	url: "load_page.php",
+	url: "http://localhost:8080/load_page.php",
 	data: 'page='+url,//with the page number as a parameter
 	dataType: "html",//expect html to be returned
 	success: function(msg){
